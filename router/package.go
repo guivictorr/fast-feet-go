9b2b032be58f2c0b782243b78fc7144c6e0bdff2
Guilherme Victor <55333929+guivictorr@users.noreply.github.com>
2024-04-10 23:12:49 -0300

    refacor: created a file just for packages routes

---
+++ router/package.go
@@ -0,0 +1,35 @@
+package router
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func initializePackageRoutes(r *gin.RouterGroup) {
+	r.GET("/packages", func(ctx *gin.Context) {
+		ctx.JSON(http.StatusOK, gin.H{
+			"message": "list packages",
+		})
+	})
+	r.GET("/packages/:id", func(ctx *gin.Context) {
+		ctx.JSON(http.StatusOK, gin.H{
+			"message": "get package " + ctx.Param("id"),
+		})
+	})
+	r.POST("/packages", func(ctx *gin.Context) {
+		ctx.JSON(http.StatusOK, gin.H{
+			"message": "create packages",
+		})
+	})
+	r.PUT("/packages/:id", func(ctx *gin.Context) {
+		ctx.JSON(http.StatusOK, gin.H{
+			"message": "update package " + ctx.Param("id"),
+		})
+	})
+	r.DELETE("/packages/:id", func(ctx *gin.Context) {
+		ctx.JSON(http.StatusOK, gin.H{
+			"message": "delete package " + ctx.Param("id"),
+		})
+	})
+}
--- router/routes.go
+++ router/routes.go
@@ -1,18 +1,10 @@
 package router
 
 import (
-	"net/http"
-
 	"github.com/gin-gonic/gin"
 )
 
 func initializeRouter(r *gin.Engine) {
 	v1 := r.Group("/api/v1")
-	{
-		v1.GET("/packages", func(ctx *gin.Context) {
-			ctx.JSON(http.StatusOK, gin.H{
-				"message": "list-packages",
-			})
-		})
-	}
+	initializePackageRoutes(v1)
 }

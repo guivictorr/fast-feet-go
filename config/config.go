4128e534b758664a6482f2f5cee7e54d313e8dbc
Guilherme Victor <55333929+guivictorr@users.noreply.github.com>
2024-04-14 20:49:44 -0300

    feat: creating a function to get sqlite instance from config

--- config/config.go
+++ config/config.go
@@ -1,6 +1,30 @@
 package config
 
-var logger *Logger
+import (
+	"fmt"
+
+	"gorm.io/gorm"
+)
+
+var (
+	logger *Logger
+	db     *gorm.DB
+)
+
+func Init() error {
+	var err error
+
+	db, err = InitializeSQLite()
+	if err != nil {
+		return fmt.Errorf("error initializing sqlite: %v", err)
+	}
+
+	return nil
+}
+
+func GetSQLite() *gorm.DB {
+	return db
+}
 
 func GetLogger(p string) *Logger {
 	logger = NewLogger(p)

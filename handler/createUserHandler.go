b0ef13720885ed80983a2607c0fd9436ac569005
Guilherme Victor <55333929+guivictorr@users.noreply.github.com>
2024-05-08 22:43:50 -0300

    feat: implementing createUserHandler

--- handler/createUserHandler.go
+++ handler/createUserHandler.go
@@ -1,7 +1,49 @@
 package handler
 
-import "github.com/gin-gonic/gin"
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"github.com/guivictorr/fast-feet-go/schemas"
+)
+
+type CreateUserRequest struct {
+	Name     string       `json:"name"`
+	Cpf      string       `json:"cpf"`
+	Password string       `json:"password"`
+	Role     schemas.Role `json:"role"`
+}
 
 func CreateUserHandler(ctx *gin.Context) {
-	println("Create User")
+	request := CreateUserRequest{}
+
+	ctx.BindJSON(&request)
+
+	// TODO: VALIDATE BODY
+
+	user := schemas.User{
+		Name:     request.Name,
+		Cpf:      request.Cpf,
+		Password: request.Password,
+		Role:     request.Role,
+	}
+
+	if err := db.Create(&user).Error; err != nil {
+		logger.Errorf("error creating user: %v", err.Error())
+		// TODO: CREATE A SENDERROR FUNC
+		ctx.Header("Content-type", "application/json")
+		ctx.JSON(http.StatusInternalServerError, gin.H{
+			"message":   "error creating user on database",
+			"errorCode": http.StatusInternalServerError,
+		})
+		return
+	}
+
+	// TODO: CREATE A SENDSUCCESS FUNC
+	ctx.Header("Content-type", "application/json")
+	ctx.JSON(http.StatusOK, gin.H{
+		"message": fmt.Sprintf("operation from handler: %s successfull", "createUser"),
+		"data":    user,
+	})
 }

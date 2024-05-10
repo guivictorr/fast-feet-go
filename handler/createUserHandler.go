7b79ac79c9d55091b5b62ef35c1a7ba8348d290f
Guilherme Victor <55333929+guivictorr@users.noreply.github.com>
2024-05-09 21:50:45 -0300

    feat: creating sendSuccess and sendError funcs

--- handler/createUserHandler.go
+++ handler/createUserHandler.go
@@ -1,7 +1,6 @@
 package handler
 
 import (
-	"fmt"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
@@ -31,19 +30,9 @@ func CreateUserHandler(ctx *gin.Context) {
 
 	if err := db.Create(&user).Error; err != nil {
 		logger.Errorf("error creating user: %v", err.Error())
-		// TODO: CREATE A SENDERROR FUNC
-		ctx.Header("Content-type", "application/json")
-		ctx.JSON(http.StatusInternalServerError, gin.H{
-			"message":   "error creating user on database",
-			"errorCode": http.StatusInternalServerError,
-		})
+		sendError(ctx, http.StatusInternalServerError, "error creating user")
 		return
 	}
 
-	// TODO: CREATE A SENDSUCCESS FUNC
-	ctx.Header("Content-type", "application/json")
-	ctx.JSON(http.StatusOK, gin.H{
-		"message": fmt.Sprintf("operation from handler: %s successfull", "createUser"),
-		"data":    user,
-	})
+	sendSuccess(ctx, user, "createUser")
 }

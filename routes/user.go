a43d9c2e799c004a0ca4f8035694b2330c05a4ef
Guilherme Victor <55333929+guivictorr@users.noreply.github.com>
2024-05-20 23:41:06 -0300

    feat: created middlewares package and auth middleware

--- routes/user.go
+++ routes/user.go
@@ -4,6 +4,7 @@ import (
 	"github.com/gin-gonic/gin"
 	userControllers "github.com/guivictorr/fast-feet-go/controllers/user-controllers"
 	userHandlers "github.com/guivictorr/fast-feet-go/handlers/user-handlers"
+	"github.com/guivictorr/fast-feet-go/middlewares"
 	"gorm.io/gorm"
 )
 
@@ -11,6 +12,8 @@ func InitUserRoutes(db *gorm.DB, route *gin.RouterGroup) {
 	userRepository := userControllers.NewUserController(db)
 	userService := userControllers.NewUserService(userRepository)
 	userHandlers := userHandlers.NewHandler(userService)
+
+	route.Use(middlewares.Auth())
 
 	route.GET("/", userHandlers.ListUsersHandler)
 	route.GET("/:id", userHandlers.FindUserHandler)
--- utils/jwt.go
+++ utils/jwt.go
@@ -1,8 +1,10 @@
 package utils
 
 import (
+	"strings"
 	"time"
 
+	"github.com/gin-gonic/gin"
 	"github.com/golang-jwt/jwt/v5"
 )
 
@@ -27,4 +29,19 @@ func Sign(data map[string]interface{}, SecretKeyEnvName string, ExpiredAt time.D
 	}
 
 	return accessToken, nil
+}
+
+func VerifyTokenHeader(ctx *gin.Context, SecrePublicKeyEnvName string) (*jwt.Token, error) {
+	tokenHeader := ctx.GetHeader("Authorization")
+	accessToken := strings.SplitAfter(tokenHeader, "Bearer")[1]
+	jwtSecretKey := "JWT_SECRET"
+
+	token, err := jwt.Parse(strings.Trim(accessToken, " "), func(token *jwt.Token) (interface{}, error) {
+		return []byte(jwtSecretKey), nil
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	return token, nil
 }
